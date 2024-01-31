cc07bdf7efce510ceb7fda5b6874febdc9dba188
adindaputrinr <novkarizky@gmail.com>
2024-01-31 20:39:30 +0700

    Verifikasi Login dan Dashboard

---
+++ cypress/e2e/Belajar/Dashboard.cy.js
@@ -0,0 +1,92 @@
+describe("Dashboard Test Suite", () => {
+    beforeEach(() => {
+     cy.clearAllSessionStorage;
+    })
+    it("Verifikasi Logo Pada Dashboard", () => {
+        cy.visit("https://demo.snipeitapp.com/login");
+        cy.get("#username").click();
+        cy.get("#username").type("admin");
+        cy.get("#password").click();
+        cy.get("#password").type("password");
+        cy.get(".form-control > input").not("[disabled]").check();
+        cy.get(".col-md-12 > a")
+          .should("be.visible")
+          .contains("I forgot my password");
+        cy.get('.btn').click();
+        cy.get('.content-header').click();
+        cy.url().should('contains', 'https://demo.snipeitapp.com/');
+        cy.get('.content-header').click();
+      });
+
+    // it("Verifikasi Sidebar Dashboard", () => {
+
+    // });s
+
+    it("Verifikasi Search Bar Dashboard", () => {
+        cy.visit("https://demo.snipeitapp.com/login");
+        cy.get("#username").click();
+        cy.get("#username").type("admin");
+        cy.get("#password").click();
+        cy.get("#password").type("password");
+        cy.get(".form-control > input").not("[disabled]").check();
+        cy.get(".col-md-12 > a")
+          .should("be.visible")
+          .contains("I forgot my password");
+        cy.get('.btn').click();
+        cy.get('.content-header').click();
+        cy.get('#tagSearch').click();
+        cy.get('#tagSearch').type('Laptops');
+        cy.get('.col-xs-1 > .btn').click();
+        cy.get('.navbar-form').submit();
+        cy.url().should('contains', 'https://demo.snipeitapp.com/hardware');
+        cy.get('.content-header').click();
+
+    });
+
+    it("Verifikasi Dropdown Create New Dashboard", () => {
+        cy.visit("https://demo.snipeitapp.com/login");
+        cy.get("#username").click();
+        cy.get("#username").type("admin");
+        cy.get("#password").click();
+        cy.get("#password").type("password");
+        cy.get(".form-control > input").not("[disabled]").check();
+        cy.get(".col-md-12 > a")
+          .should("be.visible")
+          .contains("I forgot my password");
+        cy.get('.btn').click();
+        cy.get('.content-header').click();
+        cy.get('.open > .dropdown-toggle').click();
+        cy.url().should('contains', 'https://demo.snipeitapp.com/hardware/create');
+        cy.get('.open > .dropdown-toggle').click();
+        cy.url().should('contains', 'https://demo.snipeitapp.com/licenses/create');
+        cy.get('.open > .dropdown-toggle').click();
+        cy.url().should('contains', 'https://demo.snipeitapp.com/accessories/create');
+        cy.get('.open > .dropdown-toggle').click();
+        cy.url().should('contains', 'https://demo.snipeitapp.com/consumables/create');
+        cy.get('.open > .dropdown-toggle').click();
+        cy.url().should('contains', 'https://demo.snipeitapp.com/components/create');
+        cy.get('.open > .dropdown-toggle').click();
+        cy.url().should('contains', 'https://demo.snipeitapp.com/users/create');
+
+    });
+
+    it("Verifikasi Icon Navigation Bar Dashboard", () => {
+        cy.visit("https://demo.snipeitapp.com/login");
+        cy.get("#username").click();
+        cy.get("#username").type("admin");
+        cy.get("#password").click();
+        cy.get("#password").type("password");
+        cy.get(".form-control > input").not("[disabled]").check();
+        cy.get(".col-md-12 > a")
+          .should("be.visible")
+          .contains("I forgot my password");
+        cy.get('.btn').click();
+        cy.get('.content-header').click();
+        cy.url().should('contains', 'https://demo.snipeitapp.com/hardware');
+        cy.url().should('contains', 'https://demo.snipeitapp.com/licenses');
+        cy.url().should('contains', 'https://demo.snipeitapp.com/accessories');
+        cy.url().should('contains', 'https://demo.snipeitapp.com/consumables');
+        cy.url().should('contains', 'https://demo.snipeitapp.com/components');
+
+    });
+});
---
+++ cypress/e2e/Belajar/Login.cy.js
@@ -0,0 +1,68 @@
+// // cypress e2e/login.cy.js
+// /// <reference types="cypress" />
+
+// describe("Login Test Suite", () => {
+//     beforeEach(() => {
+//         Navigation.visitLogin();
+//         cy.clearAllSessionStorage;
+//     });
+
+//     it("Verifikasi Halaman Login", () => {
+//         cy.visit('https://demo.snipeitapp.com/login');
+//         cy.get('.alert').click();
+//         cy.get('.form-group:nth-child(1) > label').click();
+//         cy.get('#username').click();
+//         cy.get('#username').click();
+//         cy.get('.form-group:nth-child(2) > label').click();
+//         cy.get('#password').click();
+//         cy.get('#password').click();
+//         cy.get('.form-control > input').click();
+//         cy.get('.form-control > input').click();
+//         cy.url().should('contains', 'https://demo.snipeitapp.com/');
+//     });
+//     });
+describe("Login", () => {
+   beforeEach(() => {
+    cy.clearAllSessionStorage;
+   })
+
+it("Verifikasi Login", () => {
+    cy.visit('https://demo.snipeitapp.com/login');
+    cy.get('.container > .row').click();
+    cy.get('#username').click();
+    cy.get('#username').type('admin');
+    cy.get('#password').click();
+    cy.get('#password').type('password');
+    cy.get('.form-control:nth-child(1)').click();
+    cy.get('.form-control > input').click();
+    cy.get('.btn').click();
+    cy.url().should('contains', 'https://demo.snipeitapp.com/');
+    cy.get('.alert');
+})
+it("Verifikasi valid user berhasil Login", () => {
+    cy.visit("https://demo.snipeitapp.com/login");
+    cy.get("#username").click();
+    cy.get("#username").type("admin");
+    cy.get("#password").click();
+    cy.get("#password").type("password");
+    cy.get(".form-control > input").not("[disabled]").check();
+    cy.get(".col-md-12 > a")
+      .should("be.visible")
+      .contains("I forgot my password");
+      cy.get('.btn').click();
+  });
+//   it("Verifikasi valid user berhasil Login", () => {
+//     cy.visit("https://demo.snipeitapp.com/login");
+//     cy.get("#username").click();
+//     cy.get("#username").type("admin");
+//     cy.get("#password").click();
+//     cy.get("#password").type("password");
+//     cy.get(".form-control > input").not("[disabled]").check();
+//     cy.get(".form-control > input")
+//       .not("[disabled]")
+//       .uncheck()
+//       .should("not.be.checked");
+//     cy.get(".col-md-12 > a")
+//       .should("be.visible")
+//       .contains("I
+});
